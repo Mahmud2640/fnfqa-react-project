659a8f4d4c9db107731b7e28e83f53b35c1685d9
Mahmud2640 <mdmahmud484@gmail.com>
2022-07-31 00:07:21 +0600

    added some employ

--- src/Components/Employe/Employe.js
+++ src/Components/Employe/Employe.js
@@ -4,45 +4,70 @@ import profile1 from "../../Images/Image/profile-1.jpeg";
 import profile2 from "../../Images/Image/profile-2.jpeg";
 import profile3 from "../../Images/Image/profile-3.jpeg";
 import profile4 from "../../Images/Image/profile-4.jpg";
+import profile5 from "../../Images/Image/profile-5.jpeg";
+import profile6 from "../../Images/Image/profile-6.jpeg";
 
 const Employe = () => {
   return (
-    <section
-      class="teams"
-      id="teams"
-      data-aos="zoom-in-left"
-      data-aos-easing="ease-out-cubic"
-      data-aos-duration="6000"
-    >
+    <section class="teams" id="teams">
       <div class="max-width">
         <h2 class="teams-title">Our Teams</h2>
         <div class="carousel owl-carousel">
           <div class="card">
             <div class="box">
               <img src={profile1} alt="" />
-              <div class="text">Ahsanul Hoque Jihad</div>
-              <p>Founder "FNF TRADING CONTRACTING AND SERVICES"</p>
+              <div class="text">
+                <strong> Ahsanul Hoque Jihad</strong>
+              </div>
+              <p>Founder</p>
             </div>
           </div>
+
           <div class="card">
             <div class="box">
               <img src={profile4} alt="" />
-              <div class="text">Abdullah Al Mahmud</div>
-              <p>Web Developer "FNF TRADING CONTRACTING AND SERVICES"</p>
+              <div class="text">
+                <strong>Abdullah Al Mahmud</strong>
+              </div>
+              <p>Web Developer</p>
             </div>
           </div>
+
           <div class="card">
             <div class="box">
               <img src={profile2} alt="" />
-              <div class="text">MD. Arafat Hossain</div>
-              <p>Representative "FNF TRADING CONTRACTING AND SERVICES"</p>
+              <div class="text">
+                <strong>MD. Arafat Hossain</strong>
+              </div>
+              <p>Representative</p>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="box">
+              <img src={profile3} alt="" />
+              <div class="text">
+                <strong>Kazi Shah Jalal Forhad</strong>
+              </div>
+              <p>Marketing Executive</p>
             </div>
           </div>
           <div class="card">
             <div class="box">
-              <img src={profile3} alt="" />
-              <div class="text">Kazi Shah Jalal Forhad</div>
-              <p>Sales Manager "FNF TRADING CONTRACTING AND SERVICES"</p>
+              <img src={profile5} alt="" />
+              <div class="text">
+                <strong>Mona Liza A. Bela-ong</strong>
+              </div>
+              <p>Marketing Executive</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="box">
+              <img src={profile6} alt="" />
+              <div class="text">
+                <strong>MD Rayhan U Shamim</strong>
+              </div>
+              <p>Marketing Representative</p>
             </div>
           </div>
         </div>
